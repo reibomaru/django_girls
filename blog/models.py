de32c540813154b7b2ee16bcf07f65d0d5ae1b29
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-03-19 13:25:37 +0900

    blogのmodelを追加した

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # 他のモデルへのリンク
+    title = models.CharField(max_length=200)
+    # 文字数が制限されたテキストを定義するフィールド
+    text = models.TextField()
+    # 制限無しの長いテキスト用です。ブログポストのコンテンツに理想的なフィールド
+    created_data = models.DateTimeField(default=timezone.now)
+    published_data = models.DateTimeField(blank=True, null=True)
+    # 日付と時間のフィールド
+    # https://docs.djangoproject.com/ja/2.2/ref/models/fields/#field-types
+
+    def publish(self):
+        self.published_data = timezone.now
+        self.save()
+    
+    def __str__(self):
+        return self.title
+
