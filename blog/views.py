344a1eec01e9b12b20d160c3339cb7cd01ef0826
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-03-20 21:59:06 +0900

    未公開ページの一覧ページと削除機能を実装した

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,18 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+
+
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == 'POST':
@@ -21,12 +25,12 @@ def post_new(request):
             #commit=FalseはPostモデルをまだ保存しないという意味です。保存前にauthorを追加したいので。
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -35,9 +39,31 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', {'post': post})
+
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
