d5e6931a9f595a9a1b9ea1813ce5b0a9d0e7325a
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-03-21 18:24:47 +0900

    投稿にコメント機能を追加した

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Post
+from .models import Post, Comments
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('author', 'text')
